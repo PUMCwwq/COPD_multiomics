cc4e480c29da7d3649fd8b88cd55ce6ec2af279c
PUMCwwq <164609568+PUMCwwq@users.noreply.github.com>
2025-04-09 16:26:24 +0800

    Update 06_Genomics_analysis.R

--- 06_Genomics_analysis/06_Genomics_analysis.R
+++ 06_Genomics_analysis/06_Genomics_analysis.R
@@ -4,53 +4,16 @@
 # Date: 2025-3-1
 ##############################################################################
 
-##### 2. Regulatory network identification
-### 2.1.
-
-
-# Chi-square test
-library(knitr)
-setwd("/NASdata_2/wuwq/COPD/OMIC_ALL_SUM/SNP")
-df = read.table('again_risk_allele_recessive.txt', header = TRUE)
-kable(df)
-
-out <- data.frame()
-for (i in 1:nrow(df)){
-  t <- chisq.test(matrix(as.vector(t(df[i, 4:9])), nrow=2, ncol=3),correct = TRUE) 
-  #t <- fisher.test(matrix(as.vector(t(df[i, 1:4])), nrow=2, ncol=2))
-  d <- df[i, ]
-  d$p.value <- t$p.value
-  d$OR <- t$estimate[[1]]
-  d$OR.lower95 <- t$conf.int[1]
-  d$OR.upper95 <- t$conf.int[2]
-  out <- rbind(out, d)
-}
-
-kable(out)
-write.csv(out,file="out_again_risk_allele_recessive.csv")
-
-
-######################################### PRS
-rm(list=ls())
-setwd("/NASdata_2/wuwq/COPD/OMIC_ALL_SUM/SNP")
+##### 1.Polygenic Risk Score (PRS) associated analysis
 library(tidyverse)
 library(readxl)
 library(ggsci)
 library(viridis)
 library(RColorBrewer)
-df_data <- read.delim("violin_PRS.txt",header = T,row.names = 1)
+# Input: PRS score and subtupe information
+df_data <- read.delim("PRS.txt",header = T,row.names = 1)
 df_data$CLUSTER_MATCH<-as.character(df_data$CLUSTER_MATCH)
-###
-theme_clean <- function(){
-  theme_bw() %+replace%    
-    theme(
-      panel.grid = element_blank(),
-      strip.background = element_blank()
-    )
-}
-
-
-########Draw a box plot
+# display the box plot
 p1<-ggplot(df_data,aes(x=CLUSTER_MATCH, y=df_data$PRS.0.001, color=CLUSTER_MATCH, fill=CLUSTER_MATCH))+
   geom_boxplot()
 p2 <- p1 +
@@ -63,6 +26,36 @@ p3 +
   scale_y_continuous(limits = c(-40,-30))+    # Agatston (0,1850), calcification (0,610), aera (14,1200)
   guides(color="none")+
   theme_clean()
+
+
+##### 2. Regulatory network identification
+### 2.1. GWAS-based subtype enrichment
+# Chi-square test
+library(knitr)
+# Input: frequencies of COPD-associated risk alleles from SNPs associated with COPD or lung function previously identified
+# odds ratios were calculated based on the actual and expected frequencies of COPD-associated risk alleles
+df = read.table('risk_allele.txt', header = TRUE)
+kable(df)
+out <- data.frame()
+for (i in 1:nrow(df)){
+  t <- chisq.test(matrix(as.vector(t(df[i, 4:9])), nrow=2, ncol=3),correct = TRUE) 
+  #t <- fisher.test(matrix(as.vector(t(df[i, 1:4])), nrow=2, ncol=2))
+  d <- df[i, ]
+  d$p.value <- t$p.value
+  d$OR <- t$estimate[[1]]
+  d$OR.lower95 <- t$conf.int[1]
+  d$OR.upper95 <- t$conf.int[2]
+  out <- rbind(out, d)
+}
+kable(out)
+write.csv(out,file="out_risk_allele.csv")
+# For each subtype, SNPs with OR > 1.2 and two-tailed Chi-square test P < 0.05 were defined as subtype-enriched SNPs (filtered in excel).
+
+### 2.2. Functional genomics information
+
+
+
+
 
 
 
