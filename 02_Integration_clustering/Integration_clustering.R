f01f217f2ffea79424474c1c2a8e430829d7fa6f
PUMCwwq <164609568+PUMCwwq@users.noreply.github.com>
2025-04-09 13:18:57 +0800

    Update Integration_clustering.R

--- 02_Integration_clustering/Integration_clustering.R
+++ 02_Integration_clustering/Integration_clustering.R
@@ -4,28 +4,21 @@
 # Date: 2025-3-1
 ##############################################################################
 
-
-######################################################### Integration
-#load data
-iris1<-read.table("/NASdata_2/wuwq/COPD/OMIC_ALL_SUM/SNF_num.txt" ,header = T,sep = "\t")
-iris2<-read.table("/NASdata_2/wuwq/COPD/OMIC_ALL_SUM/SNF_integer.txt" ,header = T,sep = "\t")
-rownames(iris1) <- iris1[,1]
+##### 1.Multi-omics integration
+# losd the data
+# iris1: filtered continuous variables including radiomics, metabolomics, and microbiomics
+# iris2: filtered categorical variables including clumped SNPs
+iris1<-read.table("num.txt" ,header = T,sep = "\t")
+iris2<-read.table("integer.txt" ,header = T,sep = "\t")
 iris1<-as.matrix(iris1)
-iris1=iris1[,2:675]
-rownames(iris2) <- iris2[,1]
 iris2<-as.matrix(iris2)
-iris2=iris2[,2:181]
 #Continuous variables were converted to numerical values
 iris1_vec=as.numeric(as.vector(iris1))
 iris1_1 <- matrix(iris1_vec, nrow = nrow(iris1))
-rownames(iris1_1)<-rownames(iris1)
-colnames(iris1_1)<-colnames(iris1)
 iris1_1<-scale(iris1_1)
 #Convert the category variable to an integer
 iris2_vec=as.integer(as.vector(iris2))
 iris2_1 <- matrix(iris2_vec, nrow = nrow(iris2))
-rownames(iris2_1)<-rownames(iris2)
-colnames(iris2_1)<-colnames(iris2)
 #combination_FINALLY
 iris3L <- list()
 iris3L[1]=list(iris1_1)
@@ -40,22 +33,24 @@ T = 20 # number of iterations of SNF
 ##### gower Distance _3 numerical omics
 gower_dist_3o <- daisy(iris1_1, metric = "gower")
 gower_dist_3o<-as.matrix(gower_dist_3o)
-write.csv(gower_dist_3o,file="/NASdata_2/wuwq/COPD/OMIC_ALL_SUM/NUM_matrix.csv")
+write.csv(gower_dist_3o,file="NUM_matrix.csv")
 ##### gower Distance_1 taxonomics
 gower_dist_1o <- daisy(iris2_1, metric = "gower")
 gower_dist_1o<-as.matrix(gower_dist_1o)
-write.csv(gower_dist_1o,file="/NASdata_2/wuwq/COPD/OMIC_ALL_SUM/SNP_matrix.csv")
+write.csv(gower_dist_1o,file="SNP_matrix.csv")
 ##########dist_iris3L
 dist_iris3 <- list()
 dist_iris3[1]=list(gower_dist_3o)
 dist_iris3[2]=list(gower_dist_1o)
 # Construct the similarity graphs
 affinity_iris3L = lapply(dist_iris3, function(x) affinityMatrix(x, K, alpha))
-write.csv(affinity_iris3L,file="/NASdata_2/wuwq/COPD/OMIC_ALL_SUM/affinity_matrix.csv")
+write.csv(affinity_iris3L,file="affinity_matrix.csv")
 W = SNF(affinity_iris3L, K, T)
-write.csv(W,file="/NASdata_2/wuwq/COPD/OMIC_ALL_SUM/W_matrix.csv")
+write.csv(W,file="W_matrix.csv")
 
-###function loading
+
+##### 2.Spectral clustering                     
+#function loading
 #SpectralClustering
 SpectralClustering <- function(affinity, K, type=3) {
   d = rowSums(affinity)
@@ -149,71 +144,32 @@ discretisationEigenVectorData <- function(eigenVector) {
   
 }
 
-###Spectral clustering results
+# Spectral clustering results, k = 2 ~ 10
 clustering = SpectralClustering(W,3)
 clustering <- as.matrix(clustering)
-write.csv(clustering, file="/NASdata_2/wuwq/COPD/OMIC_ALL_SUM/CLUSTER.csv")
-write.csv(clustering, file="/NASdata_2/wuwq/COPD/OMIC_ALL_SUM/CLUSTER1.csv")
+write.csv(clustering, file="CLUSTER1.csv")
 sil <- silhouette(clustering, W)
 fviz_silhouette(sil)
 
 
-################################################# W matrix display
-rm(list=ls())
-dev.off()
-dev.new()
+###### 3. Similarity matrix display - W matrix
 BiocManager::install("pheatmap")
 library(pheatmap)
-rm(list=ls())
-############################################### All omics affinity matrices are presented
-#########################################################Annotated heat map
-data<-read.delim("/NASdata_2/wuwq/COPD/OMIC_ALL_SUM/Affinity_heatmap.txt",header = T,row.names = 1)
-#Note that the number of columns is the same as the number of samples
-Groups<-read.table("/NASdata_2/wuwq/COPD/OMIC_ALL_SUM/groups_affinity.txt")
-rownames(Groups)<-Groups$V1
-Groups<-Groups[2:319,]
-cluster<-Groups[,2]
+# Input: the clustering result when k=3, and the similarity matrix (W_matrix)
+data<-read.delim("W_matrix.txt",header = T,row.names = 1)
+# Note that the number of columns is the same as the number of samples
+cluster<-read.table("clustering.txt")
 cluster<-as.data.frame(cluster)
-annotation_c<-cluster
-annotation_c
-rownames(annotation_c)<-rownames(Groups)
-
-
 data1<- log10(data)  
 data1<-scale(data1)
 data1<- log10(data1+10)  
 data1<-scale(data1)
-data1<- log10(data1+10)  
-data1<-scale(data1)
-data1<- log10(data1+10)  
-data1<-scale(data1)
-data1<- log10(data1+10)  
-data1<-scale(data1)
-data1<- log10(data1+10)  
-data1<-scale(data1)
-#data1<- log10(data1+10)  
-#data1<-scale(data1)
-#data1<- log10(data1+10)  
-#data1<-scale(data1)
-#data1<- log10(data1+10)  
-#data1<-scale(data1)
-#data1<- log10(data1+10)  
-#data1<-scale(data1)
-#data1<- log10(data1+10)  
-#data1<-scale(data1)
-#data1<- log10(data1+10)  
-#data1<-scale(data1)
-
 p<-pheatmap(data1, cluster_rows = F, cluster_cols = F,     #Rows are (not) clustered, columns are (not) clustered
             show_rownames = F,       #Line names are not displayed
             show_colnames = F,      #The display specifies that angle_row="15", row names are rotated by 15 degrees, and column names are similar
-            #annotation_col = annotation_c,  #To annotate a column is to group the columns
             scale= "none",             #The data were normalized by row
             border=F,               #Does not display a small lattice border
             color = colorRampPalette(c("#98CADD","#98CADD","#98CADD","#98CADD","#98CADD","#98CADD","#98CADD","#98CADD","#98CADD","#98CADD","#98CADD","#98CADD","#98CADD","#98CADD","#98CADD","#98CADD","#98CADD","#98CADD","#98CADD","#98CADD","#98CADD","#98CADD","#98CADD","#98CADD","#98CADD","#EAEFF6","#DC143C","#DC143C","#DC143C","#DC143C","#DC143C","#DC143C","#DC143C"))(100))
-#color = colorRampPalette(c("#547297","#547297","#547297","#547297","#547297","#547297","#547297","#547297","#547297","#547297","#547297","#547297","#547297","#547297","#547297","#547297","#547297","#547297","#547297","#547297","#547297","#547297","#547297","#547297","#547297","#547297","#547297","#547297","#547297","#547297","#547297","#547297","#547297","#547297","#547297","#EAEFF6","white","#DC143C","#DC143C","#DC143C","#DC143C","#DC143C","#DC143C","#DC143C","#DC143C","#DC143C","#DC143C","#DC143C","#DC143C","#DC143C","#DC143C","#DC143C","#DC143C","#DC143C"))(100))
 p 
-dev.off()
-dev.new()
 
 
