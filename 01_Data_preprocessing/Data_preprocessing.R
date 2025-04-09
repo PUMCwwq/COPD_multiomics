bba43fdd34717e8f8af9b44c4feee0592d6cc550
PUMCwwq <164609568+PUMCwwq@users.noreply.github.com>
2025-04-09 12:00:33 +0800

    Update Data_preprocessing.R

--- 01_Data_preprocessing/Data_preprocessing.R
+++ 01_Data_preprocessing/Data_preprocessing.R
@@ -35,8 +35,7 @@ iris1_vec<-scale(iris1_vec)
 # Sort the variances and select the top 20% of variables
 iris1_vec_variances <- apply(iris1_vec, 2, var)
 top_20_percent_vars <- names(sort(iris1_vec_variances, decreasing = TRUE)[1:floor(length(iris1_vec_variances) * 0.2)])
-
-iris1_1<-
+iris1_1<- iris1[, top_20_percent_vars]
 
 
 ##### 3.Categorical variables - SNPs associated with COPD or lung function were filtered.
