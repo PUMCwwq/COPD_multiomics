5e6b5b571457db381302557358ba21c769f83bee
PUMCwwq <164609568+PUMCwwq@users.noreply.github.com>
2025-04-09 11:57:20 +0800

    Update Data_preprocessing.R

--- 01_Data_preprocessing/Data_preprocessing.R
+++ 01_Data_preprocessing/Data_preprocessing.R
@@ -24,9 +24,19 @@ print(filled_dataset)
 write.csv(filled_dataset,file="CT_knn.csv")
 
 
-##### 2.Continuous variables were normalized collectively.
+##### 2.Continuous variables were scaled collectively.
+# Input: Radiomics, metabolomics, and microbiomics data
+iris1<-read.table("num.txt" ,header = T,sep = "\t")
+rownames(iris1) <- iris1[,1]
+iris1<-as.matrix(iris1)
+# Convert continuous variables into numerical values
+iris1_vec=as.numeric(as.vector(iris1))
+iris1_vec<-scale(iris1_vec)
+# Sort the variances and select the top 20% of variables
+iris1_vec_variances <- apply(iris1_vec, 2, var)
+top_20_percent_vars <- names(sort(iris1_vec_variances, decreasing = TRUE)[1:floor(length(iris1_vec_variances) * 0.2)])
 
-
+iris1_1<-
 
 
 ##### 3.Categorical variables - SNPs associated with COPD or lung function were filtered.
