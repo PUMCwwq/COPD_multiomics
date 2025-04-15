8a3e7f311ac82d51444bd3e7cad2d6219e360c88
PUMCwwq <164609568+PUMCwwq@users.noreply.github.com>
2025-04-15 10:36:46 +0800

    Add files via upload

---
+++ 01_Data_preprocessing/Data_preprocessing - 副本.R
@@ -0,0 +1,22 @@
+########## COPD multiomics ##########
+##### CT KNN
+rm(list=ls())
+################################# The KNN method was used to impute missing CT features
+BiocManager::install("TTR")
+install.packages("/NASdata_2/wuwq/COPD/packages/xts_0.14.0.tar.gz",repos = NULL)
+install.packages("/NASdata_2/wuwq/COPD/packages/quantmod_0.4.26.tar.gz",repos = NULL)
+install.packages("/NASdata_2/wuwq/COPD/packages/TTR_0.24.4.tar.gz",repos = NULL)
+install.packages("/NASdata_2/wuwq/COPD/packages/DMwR_0.4.1.tar.gz",repos = NULL)
+library(xts)
+library(TTR)
+library(quantmod)
+library(DMwR)
+#####Import data sets
+data <- read.delim("/NASdata_2/wuwq/COPD/OMIC_ALL/CT_159.txt",quote = "", 
+                   row.names = 1, 
+                   stringsAsFactors = FALSE)
+
+filled_dataset <- knnImputation(data, k = 10)
+
+print(filled_dataset)
+write.csv(filled_dataset,file="/NASdata_2/wuwq/COPD/OMIC_ALL/CT_knn.csv")
